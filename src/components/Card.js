1533a38abbb8f6da37f99e36cd9c1d8780261b31
Mj_Mon <mjmon2812@gmail.com>
2023-10-07 02:05:18 +0530

    Add files via upload

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// const Card = (props) => {
+const Card = ({ name, email, id }) => {
+	// const { name, email, id } = props;
+	return (
+		<div className='tc bg-light-green dib bf3 pa3 ma2 grow bw2 shadow-5'>
+			{/*<img alt='robots' src={`https://robohash.org/${props.id}?200x400`} />*/}
+			<img alt='robots' src={`https://robohash.org/${id}?200x400`} />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+
+
+	);
+}
+
+export default Card;
---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	// return <h1>hi</h1>
+	console.log(props);
+// 	return props.children
+	return (
+		<div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+			{props.children}
+		</div>	
+	);
+ };
+
+export default Scroll;
